3db73f9e43b76ca2b640a3cc655920250a83f47a
Amra Mhemedovic <mehmedovic.amra@yahoo.com>
2018-10-19 17:51:41 +0200

    Implementirana klasa Transfer novca

---
+++ src/TransferNovca.java
@@ -0,0 +1,55 @@
+package Bankomat;
+
+public class TransferNovca extends Racun {
+
+	private int izvorniRacun;
+	private int targetRacun;
+	private double iznosZaPrebacivanje;
+
+	public TransferNovca() {
+
+	}
+
+	public int getIzvorniRacun() {
+		return 0;
+	}
+
+	public void setIzvorniRacun(int izvorniRacun) {
+		
+	}
+
+	public int getTargetRacun() {
+		return 0;
+	}
+
+	public void setTargetRacun(int targetRacun) {
+		
+	}
+
+	public double getIznosZaPrebacivanje() {
+		return 0;
+	}
+
+	public void setIznosZaPrebacivanje(double iznosZaPrebacivanje) {
+		
+	}
+
+	public boolean provjeriValidnost() {
+		
+
+			
+
+			
+		
+
+		
+			
+
+	}
+
+	public void izvrsiTransfer() {
+		
+		}
+
+	}
+
